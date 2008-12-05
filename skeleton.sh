87932dc129ef1a8ff38eac085214ccfe8c00ea44
Brandon Calloway <brandon@scullytown.com>
2008-12-05 10:25:30 -0500

    updated skeleton with /Users/bcalloway/Sites/WebFramework

--- skeleton.sh
+++ skeleton.sh
@@ -4,32 +4,29 @@
 rm -rf .git
 
 # Create new site skeleton
-mkdir audio
-mkdir copy
-mkdir flash
-mkdir planning
-mkdir video
-mkdir site
-cd site
-mkdir images
-mkdir swf
-cd ..
-mkdir graphics
-cd graphics
-mkdir fonts
-mkdir logos
-mkdir mockups
-mkdir images
-wget http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js -O site/scripts/jquery-1.2.6.js
-wget http://jqueryjs.googlecode.com/svn/plugins/metadata/jquery.metadata.pack.js -O site/scripts/jquery.metadata.js
-wget http://jqueryjs.googlecode.com/svn/plugins/dimensions/jquery.dimensions.js -O site/scripts/jquery.dimensions.js
-wget http://jqueryjs.googlecode.com/svn/plugins/mousewheel/jquery.mousewheel.js -O site/scripts/jquery.mousewheel.js
-wget http://jqueryjs.googlecode.com/svn/plugins/jScrollPane/jScrollPane.js -O site/scripts/jScrollPane.js
-wget http://jqueryjs.googlecode.com/svn/plugins/validate/jquery.validate.pack.js -O site/scripts/jquery.validate.js
-wget http://code.google.com/p/swfobject/source/browse/trunk/swfobject/swfobject.js -O site/scripts/swfobject.js
-wget http://www.mikeindustries.com/blog/files/sifr/2.0/sifr.js -O site/scripts/sifr.js
-wget http://jqueryjs.googlecode.com/svn/plugins/jScrollPane/demo/jScrollPane.css -O site/styles/jScrollPane.css
-wget http://www.mikeindustries.com/blog/files/sifr/2.0/sIFR-screen.css -O site/styles/sIFR-screen.css
+mkdir $PWD/site
+mkdir $PWD/site/images
+mkdir $PWD/site/swf
+mkdir $PWD/audio
+mkdir $PWD/copy
+mkdir $PWD/flash
+mkdir $PWD/graphics
+mkdir $PWD/graphics/fonts
+mkdir $PWD/graphics/logos
+mkdir $PWD/graphics/mockups
+mkdir $PWD/graphics/images
+mkdir $PWD/planning
+mkdir $PWD/video
+wget http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js -O $PWD/site/scripts/jquery-1.2.6.js
+wget http://jqueryjs.googlecode.com/svn/plugins/metadata/jquery.metadata.pack.js -O $PWD/site/scripts/jquery.metadata.js
+wget http://jqueryjs.googlecode.com/svn/plugins/dimensions/jquery.dimensions.js -O $PWD/site/scripts/jquery.dimensions.js
+wget http://jqueryjs.googlecode.com/svn/plugins/mousewheel/jquery.mousewheel.js -O $PWD/site/scripts/jquery.mousewheel.js
+wget http://jqueryjs.googlecode.com/svn/plugins/jScrollPane/jScrollPane.js -O $PWD/site/scripts/jScrollPane.js
+wget http://jqueryjs.googlecode.com/svn/plugins/validate/jquery.validate.pack.js -O $PWD/site/scripts/jquery.validate.js
+wget http://code.google.com/p/swfobject/source/browse/trunk/swfobject/swfobject.js -O $PWD/site/scripts/swfobject.js
+wget http://www.mikeindustries.com/blog/files/sifr/2.0/sifr.js -O $PWD/site/scripts/sifr.js
+wget http://jqueryjs.googlecode.com/svn/plugins/jScrollPane/demo/jScrollPane.css -O $PWD/site/styles/jScrollPane.css
+wget http://www.mikeindustries.com/blog/files/sifr/2.0/sIFR-screen.css -O $PWD/site/styles/sIFR-screen.css
 git init
 git add .
 git commit -a -m "initial commit"
